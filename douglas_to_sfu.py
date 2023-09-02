8e0d67aad9415b6bd83d35eb7db50c57307f1ebc
Toma Ozarchevici <to.m.a.a@icloud.com>
2023-09-01 20:27:27 -0700

    Added douglas_to_sfu

---
+++ douglas_to_sfu.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import sys
+import time
+
+html_text = requests.get(f'https://www.douglascollege.ca/course/{sys.argv[1].lower()}-{sys.argv[2]}').text
+soup = BeautifulSoup(html_text, 'lxml')
+transfers = soup.find('div', class_='transfers')
+
+for transfer in transfers:
+    if 'SFU' in transfer.text:
+        equivalent = re.search('(SFU \w\w\w?\w? \d\d\d\w?)', transfer.text)
+        print(equivalent[0])
